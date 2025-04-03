9628e62adf0f1d094f5c833f42a1e49ec3f840be
eduardo <eduardovanbenthum@gmail.com>
2025-04-03 22:27:08 +0200

    cleaned up code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -22,10 +22,3 @@ fun main() {
     // Need to wait for the data to be sent through all nodes to the other endpoint
     Thread.sleep(1000)
 }
-
-Enter your message: e1
-node 8003 has received message: RelayMessage(message=RelayMessage(message=RelayMessage(message=MessageContent(textMessage=e1), forwardPort=8000), forwardPort=8001), forwardPort=8002).
-node 8002 has received message: RelayMessage(message=RelayMessage(message=MessageContent(textMessage=e1), forwardPort=8000), forwardPort=8001).
-node 8001 has received message: RelayMessage(message=MessageContent(textMessage=e1), forwardPort=8000).
-node 8000 has received message: MessageContent(textMessage=e1).
-Endpoint on port 9001 has received message: e1.
--- src/main/kotlin/Node.kt
+++ src/main/kotlin/Node.kt
@@ -3,11 +3,6 @@ package han.swa.herkansing
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.modules.SerializersModule
-import kotlinx.serialization.modules.polymorphic
-import kotlinx.serialization.modules.subclass
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.ServerSocket
